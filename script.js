3d547d6b812930cca2a17d40da3a38847587a6eb
Danial2130 <danialarshad2103@gmail.com>
2026-07-28 11:09:55 +0700

    add new task and icon delete

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ document.querySelector('#push').onclick = function (){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task")
     }
+
+else{
+    document.querySelector('#task').innerHTML+= `
+    <div class="task">
+        <span id="taskname">
+            ${document.querySelector('#newtask input').value}
+        </span>
+        <button class="delete">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    </div>
+    `;
+
+    } 
 }
