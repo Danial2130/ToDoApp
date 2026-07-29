daf6db8c28647a8ab1f3f21f14dfb34407f17e8c
Danial2130 <danialarshad2103@gmail.com>
2026-07-29 07:33:17 +0700

    delete task feature

--- script.js
+++ script.js
@@ -16,4 +16,11 @@ else{
     `;
 
     } 
+    var current_tasks = document.querySelectorAll(".delete");
+    for(var i=0; i<current_tasks.length; i++){
+        current_tasks[i].onclick = function(){
+            this.parentNode.remove();
+        }
+    }
+    
 }
